d6f5d69e308f9584ec1c627dea65a761f679afad
dioexile <45012858+dioexile@users.noreply.github.com>
2022-04-12 21:51:46 +0300

    Add files via upload

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,10 @@
 import TextField from '@mui/material/TextField';
 
-const Search = () => {
+const Search = (props) => {
+    const { onChange, value } = props;
     return (
 
-        <TextField id="filled-basic" label="search" variant="filled" sx={{mb: '50px'}}
-        />
-
-
+        <TextField id="filled-basic" label="search" variant="filled" sx={{mb: '50px'}} value={value} onChange={onChange}/>
     )
 };
 
